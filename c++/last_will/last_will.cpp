4b3ca0c2347812a74f208a84f14e7441dd19f396
nelson-dylan <dyl.nel.biz@gmail.com>
2025-03-07 19:08:49 -0700

    Create last_will.cpp

---
+++ c++/last_will/last_will.cpp
@@ -0,0 +1,59 @@
+// Enter your code below the lines of the families' information
+
+// Secret knowledge of the Zhang family:
+namespace zhang {
+    int bank_number_part(int secret_modifier) {
+        int zhang_part{ 8'541 };
+        return (zhang_part * secret_modifier) % 10000;
+    }
+    namespace red {
+        int code_fragment() { return 512; }
+    }
+    namespace blue {
+        int code_fragment() { return 677; }
+    }
+}
+
+// Secret knowledge of the Khan family:
+namespace khan {
+    int bank_number_part(int secret_modifier) {
+        int khan_part{ 4'142 };
+        return (khan_part * secret_modifier) % 10000;
+    }
+    namespace red {
+        int code_fragment() { return 148; }
+    }
+    namespace blue {
+        int code_fragment() { return 875; }
+    }
+}
+
+// Secret knowledge of the Garcia family:
+namespace garcia {
+    int bank_number_part(int secret_modifier) {
+        int garcia_part{ 4'023 };
+        return (garcia_part * secret_modifier) % 10000;
+    }
+    namespace red {
+        int code_fragment() { return 118; }
+    }
+    namespace blue {
+        int code_fragment() { return 923; }
+    }
+}
+
+// Enter your code below ------------------------------------------------------------
+namespace estate_executor {
+    int assemble_account_number(int secret_modifier) {
+        // sum the families bank number parts with the secret modifier
+        int secret_account_number{ zhang::bank_number_part(secret_modifier) + khan::bank_number_part(secret_modifier) + garcia::bank_number_part(secret_modifier) };
+        return secret_account_number;
+    }
+    int assemble_code() {
+        // sum the red fragments and blue fragments seperately, then multiply the totals
+        int red{ zhang::red::code_fragment() + khan::red::code_fragment() + garcia::red::code_fragment() };
+        int blue{ zhang::blue::code_fragment() + khan::blue::code_fragment() + garcia::blue::code_fragment() };
+        return red * blue;
+    }
+
+}
