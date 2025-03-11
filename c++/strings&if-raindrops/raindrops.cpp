b1e08d0f220458f74af13f337b73c9ae0b6f33c6
Dylan Nelson <dyl.nel.biz@gmail.com>
2025-03-11 10:44:15 -0700

    Update raindrops.cpp
    Add in proper code

--- c++/strings&if-raindrops/raindrops.cpp
+++ c++/strings&if-raindrops/raindrops.cpp
@@ -1,31 +1,28 @@
-#include <string>
+#include "raindrops.h"
 
-namespace log_line {
-    std::string message(std::string line) {
-        // return the message-------------------------------
-        // locate beginning of message, add 2 to start at beggining of message
-        int colon = line.find(":") + 2;
-        // return substring starting at beginning of message
-        std::string newString = line.substr(colon);
-        return newString;
+namespace raindrops {
+    // conversion method
+    std::string convert(int a){
+        // define output string
+        std::string b;
+        // condition 1 - Pling
+        if(a % 3 == 0){
+            b += "Pling";
+        }
+        // condition 2 - Plang
+        if(a % 5 == 0){
+            b += "Plang";
+        }
+        // condition 3 - Plong
+        if(a % 7 == 0){
+            b += "Plong";
+        }
+        // condition 4 - None
+        // If string contents are not added from step 1-3, return the number in the string
+        if(b.empty()){
+            b = std::to_string(a);
+        }
+        return b;
     }
-
-    std::string log_level(std::string line) {
-        // return the log level-----------------------------
-        // find the brackets and adjust by 1, up and down, to exclude them from substring
-        int openBracket = line.find("[") + 1;
-        int closeBracket = line.find("]") - 1;
-        return line.substr(openBracket, closeBracket);
-    }
-
-    std::string reformat(std::string line) {
-        // return the reformatted message-------------------
-        /* More work
-        std::string mess = message(line);
-        std::string log = log_level(line);
-        return mess + " (" + log + ")";
-        */
-        // Less work vvv
-        return message(line) + " (" + log_level(line) + ")";
-    }
-}
+    
+}  // namespace raindrops
